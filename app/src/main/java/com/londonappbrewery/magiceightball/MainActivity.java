12572d5761131891356e0f67246f5e0cc9bd3703
Vojtech Obrusnik <v.obrusnik@gmail.com>
2017-10-14 19:49:28 +0200

    Update: Balls and buttons: Some random magic with balls..

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton;
+        final ImageView ballImage;
+        final int[] ballsList = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5,
+        };
+        final int BALLS_IMAGE_COUNT = 5;
+
+        askButton = (Button) findViewById(R.id.button_ask);
+        ballImage = (ImageView) findViewById(R.id.image_ball);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random generator = new Random();
+                int i = generator.nextInt(BALLS_IMAGE_COUNT);
+
+                ballImage.setImageResource(ballsList[i]);
+            }
+        });
     }
 }
